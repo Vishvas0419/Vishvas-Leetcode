b9bbe20820dab2b4121eb0498bd37232a8bad205
Vishvas Vaglay <158127207+Vishvas0419@users.noreply.github.com>
2025-01-09 12:25:16 +0530

    Time: 0 ms (100%), Space: 40.5 MB (86.53%) - LeetHub

---
+++ 2571-find-the-pivot-integer/2571-find-the-pivot-integer.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int pivotInteger(int n) {
+        int totalSum = n*(n+1)/2;
+        int x = (int) Math.sqrt(totalSum);
+        if(x*x != totalSum){
+            return -1;
+        }
+        return x; 
+    }
+}
