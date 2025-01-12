6dcc2709fb1403a571b0cafe26d10d4be49ee7f5
Vishvas Vaglay <158127207+Vishvas0419@users.noreply.github.com>
2025-01-12 23:15:18 +0530

    Time: 9 ms (24.14%), Space: 40.4 MB (59.02%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int fib(int n) {
+             if(n==1 || n==0){
+            return n;
+        }
+        int last = fib(n-1);
+        int second_last = fib(n-2);
+        return last + second_last;         
+    }
+}
