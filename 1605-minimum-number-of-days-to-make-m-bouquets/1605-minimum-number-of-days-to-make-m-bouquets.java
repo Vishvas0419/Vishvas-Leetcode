230ea61ce59d2f8b99d7c24c1620c879ac82bbed
Vishvas Vaglay <158127207+Vishvas0419@users.noreply.github.com>
2025-02-27 17:37:26 +0530

    Time: 19 ms (63.61%), Space: 59.1 MB (10.23%) - LeetHub

--- 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.java
@@ -2,61 +2,52 @@ class Solution {
     public int minDays(int[] bloomDay, int m, int k) {
         int n = bloomDay.length;
         long val = (long) m * k;
-        if(n<val) return -1;
-        
+        if (n < val) return -1; // Not enough flowers to make bouquets
+
         int low = minElem(bloomDay);
         int high = maxElem(bloomDay);
-        int ans = high;
-        while(low<=high)
-        {
-            int mid = low+(high-low)/2;
-            if(possible(bloomDay,mid,m,k))
-            {
-                high = mid-1; 
-            }
-            else{
-                low = mid+1;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (canMakeBouquets(bloomDay, mid, m, k)) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
         return low;
     }
-    public boolean possible(int[]nums,int day,int m,int k)
-    {
+
+    private boolean canMakeBouquets(int[] nums, int day, int m, int k) {
         int count = 0;
-        int no_of_Bouques = 0;
-        for(int i=0;i<nums.length;i++)
-        {
-            if(nums[i] <= day) //blooming is possible
-            {
-                count++; //count consecutive possible blooming Days
-            }
-            else{
-                no_of_Bouques += count/k;
+        int noOfBouquets = 0;
+
+        for (int num : nums) {
+            if (num <= day) {
+                count++; // Count consecutive blooming flowers
+                if (count == k) { // If we form a bouquet
+                    noOfBouquets++;
+                    count = 0;
+                }
+            } else {
                 count = 0;
             }
         }
-        no_of_Bouques += count/k;
-        if(no_of_Bouques >= m) return true;
-        else return false;
-
+        return noOfBouquets >= m;
     }
-    public int minElem(int[]arr)
-    {
+
+    private int minElem(int[] arr) {
         int min = Integer.MAX_VALUE;
-        int n = arr.length;
-        for(int i=0;i<n;i++)
-        {
-            min = Math.min(min,arr[i]);
+        for (int num : arr) {
+            min = Math.min(min, num);
         }
         return min;
     }
-    public int maxElem(int[]arr)
-    {
+
+    private int maxElem(int[] arr) {
         int max = Integer.MIN_VALUE;
-        int n = arr.length;
-        for(int i=0;i<n;i++)
-        {
-            max = Math.max(max,arr[i]);
+        for (int num : arr) {
+            max = Math.max(max, num);
         }
         return max;
     }
