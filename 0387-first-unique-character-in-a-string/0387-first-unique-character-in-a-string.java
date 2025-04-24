ce473df2127b74a733b474c20d917605f7ad516a
Vishvas Vaglay <158127207+Vishvas0419@users.noreply.github.com>
2025-04-25 00:30:35 +0530

    Time: 35 ms (16.86%), Space: 45.5 MB (19.11%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -7,6 +7,7 @@ class Solution {
             char ch = s.charAt(i);
             mpp.put(ch,mpp.getOrDefault(ch,0)+1);
         }
+        //check key(char) whose value(count) is 1
         for(int i=0;i<s.length();i++)
         {
             if(mpp.get(s.charAt(i)) == 1) 
