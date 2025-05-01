4d4cc12f68824f2b542f30246b9a494abd3abba1
Vishvas Vaglay <158127207+Vishvas0419@users.noreply.github.com>
2025-05-01 17:52:30 +0530

    Time: 5 ms (72.66%), Space: 44.4 MB (16.46%) - LeetHub

--- 2116-count-number-of-pairs-with-absolute-difference-k/2116-count-number-of-pairs-with-absolute-difference-k.java
+++ 2116-count-number-of-pairs-with-absolute-difference-k/2116-count-number-of-pairs-with-absolute-difference-k.java
@@ -1,10 +1,23 @@
 class Solution {
     public int countKDifference(int[] nums, int k) {
+        // int cnt = 0;
+        // for(int i = 0; i < nums.length; i++){
+        //     for(int j = i + 1 ; j < nums.length; j++){
+        //         if(Math.abs(nums[i] - nums[j]) == k) cnt++;
+        //     }
+        // }
+        // return cnt;
+
         int cnt = 0;
-        for(int i = 0; i < nums.length; i++){
-            for(int j = i + 1 ; j < nums.length; j++){
-                if(Math.abs(nums[i] - nums[j]) == k) cnt++;
+        Map<Integer, Integer> mpp = new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+            if(mpp.containsKey(k + nums[i])){
+                cnt += mpp.get(k+nums[i]);
             }
+            if(mpp.containsKey(nums[i] - k)){
+                cnt += mpp.get(nums[i] -k);
+            }
+            mpp.put(nums[i], mpp.getOrDefault(nums[i], 0) + 1);
         }
         return cnt;
     }
