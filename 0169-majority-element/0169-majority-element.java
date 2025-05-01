2c3ab727aa62cf5ecba919ab1646c254f2acb5d0
Vishvas Vaglay <158127207+Vishvas0419@users.noreply.github.com>
2025-05-02 00:13:18 +0530

    Time: 17 ms (14.33%), Space: 50 MB (73.76%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,22 +1,13 @@
 class Solution {
     public int majorityElement(int[] nums) {
+        Map<Integer,Integer>map = new HashMap<>();
         int n = nums.length;
-        //apply moore's voting algorithm
-        int count = 0;
-        int elem = 0;
-        for(int i=0;i<n;i++)
+        for(int num : nums)
         {
-            if(count==0) elem = nums[i];
-            if(elem==nums[i]) count++;
-            else count--;
-        }   
-        // verify that current elem is our majority element by checking if count>n/2
-        count=0;
-        for(int i=0;i<n;i++)
-        {
-            if(nums[i]==elem) count++;
+            map.put(num,map.getOrDefault(num,0)+1);
+            if(map.get(num)>n/2) return num;
         }
-        if(count > n/2) return elem;
         return -1;
+        
     }
 }
