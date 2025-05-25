286c857c47b1a1a3cc97ecc8b89b920b8c7e814c
Vishvas Vaglay <158127207+Vishvas0419@users.noreply.github.com>
2025-05-25 18:17:31 +0530

    Time: 6 ms (26.98%), Space: 63.8 MB (11.06%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
@@ -18,9 +18,11 @@ class Solution {
         if(root==null) return 0;
         int leftHeight = minDepth(root.left);
         int rightHeight = minDepth(root.right);
+        //if left is null recurse only rightHeight
         if(root.left==null) return 1+rightHeight;
+        //if right is null recurse only leftHeight
         if(root.right==null) return 1+leftHeight;
+        //if both childs exist, take min of the two
         return 1+Math.min(leftHeight,rightHeight);
-        
     }
 }
