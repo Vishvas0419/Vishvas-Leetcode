193dc3163d8a45efccfb30427f85d48e888a7e64
Vishvas Vaglay <158127207+Vishvas0419@users.noreply.github.com>
2025-06-05 12:19:00 +0530

    Time: 0 ms (100%), Space: 41.5 MB (65.43%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
@@ -4,9 +4,9 @@ class Solution {
         for(char ch : s.toCharArray())
         {
             if(ch=='(') open++;
-            else {
-                if(open>0) open--;
-                else closed++;
+            else { //if ch == ')'
+                if(open>0) open--; //we find a pair reduce open brackets count
+                else closed++; //else there is no open brackets for this closing brackets , increase count of closed brackets 
             }
         }
         return open+closed;
