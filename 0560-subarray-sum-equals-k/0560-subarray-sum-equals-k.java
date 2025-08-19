e276be829a92f44a90121ec9241c00a74337ab07
Vishvas Vaglay <158127207+Vishvas0419@users.noreply.github.com>
2025-08-19 12:45:19 +0530

    Time: 23 ms (92.53%), Space: 47.1 MB (10.73%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,14 +1,12 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        //HashMap + Prefix Sum (complement method)
-        Map<Integer,Integer> map = new HashMap<>();
-        //key = sum , value = freq of the sums
+        HashMap<Integer,Integer> map = new HashMap<>();
         map.put(0,1);
         int sum = 0;
         int count = 0;
-        for(int num:nums)
+        for(int num : nums)
         {
-            sum+=num;
+            sum += num;
             if(map.containsKey(sum-k))
             {
                 count += map.get(sum-k);
