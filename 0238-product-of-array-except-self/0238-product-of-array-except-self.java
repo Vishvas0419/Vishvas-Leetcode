d7d5b39b362ad1283cef3f58f1df1f67a5d302b3
Vishvas Vaglay <158127207+Vishvas0419@users.noreply.github.com>
2025-09-10 17:17:02 +0530

    Time: 2 ms (87.2%), Space: 55.2 MB (95.15%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,23 +1,31 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int n = nums.length; 
-        int [] result = new int[n];
-        int zeroes = 0; int product = 1;
-        for(int num : nums)
+        int n = nums.length;
+        int [] result = new int[n];   
+        int zeroes = 0;
+        int product = 1; 
+        for(int num:nums)
         {
             if(num==0) zeroes++;
             else product *= num;
         }
-        for(int i=0;i<n;i++)
+        for(int i=0;i<nums.length;i++)
         {
-            if(zeroes>1) result[i] = 0;
+            if(zeroes>1)
+            {
+                result[i] = 0;
+            }
             else if(zeroes==1)
             {
-                if(nums[i]==0) result[i] = product;
-                else result[i] = 0; 
+                if(nums[i] == 0)
+                {
+                    result[i] = product;
+                }
+                else result[i] = 0;
             }
             else result[i] = product/nums[i];
         }
         return result;
+
     }
 }
