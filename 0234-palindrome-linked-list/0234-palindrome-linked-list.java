c46cf7d7af6605b23eec66cc218b3af2c89e2bfd
Vishvas Vaglay <158127207+Vishvas0419@users.noreply.github.com>
2025-10-16 00:34:22 +0530

    Time: 7 ms (36.71%), Space: 56.2 MB (93.66%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -7,47 +7,25 @@
  *     ListNode(int val) { this.val = val; }
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
- 1. Find middle node
- 2.reverse from middle.next node till last node
- 3.now check for both halves data if not equal then first reverse the ll to its orginal form then return false else return true if any of the heads reaches null
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
-        while(fast.next!=null&&fast.next.next!=null)
-        {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-        ListNode first = head; 
-        ListNode newHead = reverse(slow.next);
-        ListNode second = newHead; 
-        while(second!=null)
-        {
-            if(first.val!=second.val)
-            {
-                reverse(newHead);
-                return false;
-            }
-            first = first.next;
-            second = second.next;
-        }        
-        reverse(newHead);
-        return true;
-    }
-     public ListNode reverse(ListNode head)
-    {
+        List<Integer> list = new ArrayList<>();
         ListNode temp = head;
-        ListNode prev = null;
         while(temp!=null)
         {
-            ListNode front = temp.next;
-            temp.next = prev;
-            prev = temp;
-            temp = front; //or temp.next
+            list.add(temp.val);
+            temp = temp.next;
         }
-        return prev;
+        int i=0;int j=list.size()-1;
+        while(i<j)
+        {
+            if(!(list.get(i) == list.get(j))) 
+            {
+                return false;
+            }
+            i++;j--;
+        }
+        return true;
     }
-  
 }
