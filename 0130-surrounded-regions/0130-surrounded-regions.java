8dff38cd4d8cd2c172df26bd35bceab1cc1c2fbc
Vishvas Vaglay <158127207+Vishvas0419@users.noreply.github.com>
2025-12-07 16:01:44 +0530

    Time: 2 ms (81.78%), Space: 47.7 MB (32.57%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -4,19 +4,19 @@ class Solution {
         int n = board.length;
         int m = board[0].length;
         boolean [][] vis = new boolean[n][m];
-        int[]delrow = {-1,0,+1,0};
-        int[]delcol = {0,1,0,-1};
+        // int[]delrow = {-1,0,+1,0};
+        // int[]delcol = {0,1,0,-1};
         for(int i=0;i<m;i++)
         {
             //traverse 1st row
             if(!vis[0][i]  && board[0][i]=='O')
             {
-                dfs(0,i,vis,board,delrow,delcol);
+                dfs(0,i,vis,board);
             }
             //traverse last row
             if(!vis[n-1][i]  && board[n-1][i]=='O')
             {
-                dfs(n-1,i,vis,board,delrow,delcol);
+                dfs(n-1,i,vis,board);
             }
         }
         for(int i=0;i<n;i++)
@@ -24,12 +24,12 @@ class Solution {
             //traverse 1st col
             if(!vis[i][0]  && board[i][0]=='O')
             {
-                dfs(i,0,vis,board,delrow,delcol);
+                dfs(i,0,vis,board);
             }
             //traverse last col
             if(!vis[i][m-1]  && board[i][m-1]=='O')
             {
-                dfs(i,m-1,vis,board,delrow,delcol);
+                dfs(i,m-1,vis,board);
             }
         }
         for(int i=0;i<n;i++)
@@ -44,26 +44,24 @@ class Solution {
         }
 
     }
-    static void dfs(int row,int col,boolean[][]vis,char[][]board,int[]delrow,int[]delcol)
+    static void dfs(int row,int col,boolean[][]vis,char[][]board)
     {
         vis[row][col]=true;
         int n = board.length;
         int m = board[0].length;
-        // int []dirs = {{-1,0},{1,0},{0,1},{0,-1}};
-        // for(int []d : dirs)
-        // {
-        //     int nr = row+d[0];
-        //     int nc = col+d[1];
-        // }
-
-        //for up,down,right,left
-        for(int i=0;i<4;i++)
+        int [][]dirs = {{-1,0},{1,0},{0,1},{0,-1}};
+        for(int []d : dirs)
         {
-            int nr = row + delrow[i];
-            int nc = col + delcol[i];
+            int nr = row+d[0];
+            int nc = col+d[1];
+        //for up,down,right,left
+        // for(int i=0;i<4;i++)
+        // {
+            // int nr = row + delrow[i];
+            // int nc = col + delcol[i];
             if(nr>=0 && nc>=0 && nr<n && nc<m && board[nr][nc]=='O' && !vis[nr][nc])
             {
-                dfs(nr,nc,vis,board,delrow,delcol);
+                dfs(nr,nc,vis,board);
             }
         }
     }
