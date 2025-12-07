ee2af05eada26f9ff26d3e2cade8abdfdf9929d8
Vishvas Vaglay <158127207+Vishvas0419@users.noreply.github.com>
2025-12-08 01:54:24 +0530

    Time: 8 ms (8.44%), Space: 46.9 MB (54.1%) - LeetHub

---
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -0,0 +1,44 @@
+class Solution {
+    public boolean isBipartite(int[][] graph) {
+        List<List<Integer>> adj = new ArrayList<>();
+        int n = graph.length;
+        for(int i=0;i<n;i++)
+        {
+            adj.add(new ArrayList<>());
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int u : graph[i])
+            {
+                adj.get(i).add(u);
+            }
+        }
+        int[]color = new int[n];
+        for(int i=0;i<n;i++)
+        {
+            color[i] = -1;
+        } 
+        for(int i=0;i<n;i++)
+        {
+            if(color[i]==-1)
+            {
+                if(!dfs(i,0,color,adj))return false;;
+            }
+        }
+        return true;
+    }
+    boolean dfs(int node,int col,int[] color,List<List<Integer>> adj)
+    {
+        color[node] = col;
+        for(int nbr : adj.get(node))
+        {
+            if(color[nbr]==-1)
+            {
+                if(!dfs(nbr,1-col,color,adj)) return false;
+            }
+            //if this curr node is prev colored and have color as same as the color that we have (col), then return false beacuse their cant be two adj nodes with same color  
+            else if(color[nbr]==col) return false;
+        }
+        return true;
+    }
+}
