81d6daa23ea39c220167bbce7f9563e6766cde3d
Vishvas Vaglay <158127207+Vishvas0419@users.noreply.github.com>
2026-01-16 11:25:50 +0530

    Time: 0 ms (100%), Space: 61.5 MB (35.31%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -4,12 +4,9 @@ class Solution {
         if(n==1) return;
         k=k%n;
         if(k>n) return;
-        //reverse first k numbers
-        reverse(nums,0,n-k-1);
-        //reverse last n-k numbers
-        reverse(nums,n-k,n-1);
-        //finally reverse whole array
         reverse(nums,0,n-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,n-1);
     }
     public void reverse(int[] nums,int left,int right)
     {
