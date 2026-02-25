c1ee98a594c243005e3190326621dffff7cd1522
Vishvas Vaglay <158127207+Vishvas0419@users.noreply.github.com>
2026-02-25 13:55:19 +0530

    Time: 17 ms (53.18%), Space: 47.4 MB (49.15%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -2,40 +2,45 @@ class Solution {
     public String minWindow(String s, String t) {
           if (s.length() == 0 || t.length() == 0 || s.length() < t.length()) return "";
         HashMap<Character,Integer> map = new HashMap<>();
+        //store freq of 't' chars that we want to match
         for(char ch : t.toCharArray())
         {
             map.put(ch,map.getOrDefault(ch,0)+1);
         }
-        int count = map.size();
+        int count = map.size(); //to track uniq chars
         int i=0,j=0,minLen = Integer.MAX_VALUE,startIndex=-1;
-
-        // String minSubStr = "";
         while(j<s.length())
         {
             char ch = s.charAt(j);
+            //if jth char is present in map freq[j]--;
             if(map.containsKey(ch))
             {
                 map.put(ch,map.getOrDefault(ch,0)-1);
+//If char count=0,we fulfill need for unique char
                 if(map.get(ch)==0)
                 {
                     count--;
                 }
             }
+        // When count=0, a potential ans (valid window) is found    
             if(count==0)
             {
+                // Try to min the window by sliding 'i'
                 while(count==0)
                 {
-                    if(j-i+1<minLen)
+                    if(j-i+1<minLen) //update minLen
                     {
                         minLen = j-i+1;
                         startIndex = i;       
                     }
+                    //reverse the calculations done for 'i'th char
                     if(map.containsKey(s.charAt(i)))
                     {
                         map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+// If the count of a required character goes above 0, the window is no longer valid 
                     if(map.get(s.charAt(i))>0) count++;
                 }
-                i++;
+                i++; 
                 }
             }
             j++;
