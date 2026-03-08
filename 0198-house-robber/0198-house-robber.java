1828888f18144e2ca7d5b9ca5a755fca703a5ae3
Vishvas Vaglay <158127207+Vishvas0419@users.noreply.github.com>
2026-03-08 18:59:39 +0530

    Time: 0 ms (100%), Space: 42.5 MB (93.95%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,18 +1,20 @@
 class Solution {
     public int rob(int[] nums) {
-        //tabulation code in space = O(N)
+        //tabulation code (optimized) space = O(N)
         int n = nums.length;
         int[]dp = new int[n];
         if(n==1) return nums[0];
-        //store base cases in dp array
-        dp[0] = nums[0]; //if we have one house to rob
-        dp[1] = Math.max(nums[0],nums[1]); //if we have 2 houses to rob we will take the max one
+        //store base cases in variables instead of dp array
+       int prev2 = nums[0]; //if we have one house to rob
+        int prev = Math.max(nums[0],nums[1]); //if we have 2 houses to rob we will take the max one
         for(int i=2;i<n;i++)
         {
-            int pick = nums[i] + dp[i-2];
-            int notPick = 0 + dp[i-1];
-            dp[i]  = Math.max(pick,notPick);
+            int pick = nums[i] + prev2;
+            int notPick = 0 + prev;
+            int curr = Math.max(pick,notPick);
+            prev2 = prev;
+            prev = curr;
         }
-        return dp[n-1];
+        return prev;
     }
 }
