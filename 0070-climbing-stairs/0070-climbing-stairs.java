599a3d15d3bae10698474e03171014c7c6c4fe1c
Vishvas Vaglay <158127207+Vishvas0419@users.noreply.github.com>
2026-03-21 00:01:54 +0530

    Time: 0 ms (100%), Space: 42.1 MB (49.18%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,7 +1,7 @@
 class Solution {
     public int climbStairs(int n) {
         int[] dp = new int[n+1];
-        dp[0] = 1; dp[1] = 1;
+        dp[0] = 1; dp[1] = 1; //store base cases first(tabulation - bottom-up approach)
         for(int i=2;i<=n;i++)
         {
             dp[i] = dp[i-1] + dp[i-2];
