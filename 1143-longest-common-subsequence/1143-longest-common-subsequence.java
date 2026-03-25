4c04e008bfd4af8fe193b9ab257be0bb8b2ab477
Vishvas Vaglay <158127207+Vishvas0419@users.noreply.github.com>
2026-03-26 03:19:57 +0530

    Time: 20 ms (78.4%), Space: 53.9 MB (62.45%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,22 +1,20 @@
 class Solution {
-    static int[][]dp = new int[1001][1001];
     public int longestCommonSubsequence(String text1, String text2) {
         int m = text1.length(); int n = text2.length();
-        for(int i=0;i<1001;i++) Arrays.fill(dp[i],-1);
-        return solve(text1,text2,m,n);
-    }
-    private static int solve(String x,String y,int m,int n)
-    {
-        if(m==0||n==0) return 0;
-        if(dp[m][n]!=-1) return dp[m][n]; 
-        if(x.charAt(m-1)==y.charAt(n-1))
+        int[][] dp = new int[m+1][n+1];
+        for(int i=1;i<=m;i++)
         {
-            return dp[m][n] = 1+solve(x,y,m-1,n-1);
+            for(int j=1;j<=n;j++)
+            {
+                if(text1.charAt(i-1)==text2.charAt(j-1))
+                {
+                    dp[i][j] = 1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = Math.max(dp[i][j-1],dp[i-1][j]);
+                }
+            }
         }
-        else{
-            int skipLastCharOfY = solve(x,y,m,n-1);
-            int skipLastCharOfX = solve(x,y,m-1,n);
-            return dp[m][n] = Math.max(skipLastCharOfY,skipLastCharOfX);
-        }
+        return dp[m][n];
     }
 }
