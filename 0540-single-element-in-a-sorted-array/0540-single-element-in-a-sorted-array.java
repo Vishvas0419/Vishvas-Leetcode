7d018323aad156616392d2952bcaf3ee6cf6333b
Vishvas Vaglay <158127207+Vishvas0419@users.noreply.github.com>
2026-04-07 13:56:07 +0530

    Time: 2 ms (8.87%), Space: 52.7 MB (82.64%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,19 +1,10 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int n = nums.length;
-        if (n == 1) return nums[0];
-        for (int i = 0; i < n; i++) {
-            // Check the first element
-            if (i == 0 && nums[i] != nums[i + 1]) return nums[i];
-            // Check the last element
-            else if (i == n - 1 && nums[i] != nums[i - 1]) return nums[i];
-            // Check middle elements
-            else if (i > 0 && i < n - 1) {
-                if (nums[i - 1] != nums[i] && nums[i] != nums[i + 1]) {
-                    return nums[i];
-                }
-            }
+        int XOR = 0;
+        for(int i=0;i<nums.length;i++)
+        {
+            XOR = XOR^nums[i];
         }
-        return -1;
+        return XOR;
     }
 }
