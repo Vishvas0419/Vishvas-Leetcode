43c72ded1105e33be91efbfcdee4d5c68f5333b5
Vishvas Vaglay <158127207+Vishvas0419@users.noreply.github.com>
2026-05-04 14:30:06 +0530

    Time: 5 ms (82.94%), Space: 77.5 MB (12.49%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,11 +1,9 @@
 class Solution {
     public int maxArea(int[] height) {
         //area of rectangle  = length * breadth
-        //here width = height.length , length = height[i]
-        int maxArea = Integer.MIN_VALUE;
-        int area = 1;
-        int left=0;
-        int right=height.length-1;
+        //here width = diff btw indices , length = min(height[left],height[right])
+        int maxArea = Integer.MIN_VALUE; int area = 1;
+        int left = 0; int right = height.length-1;
         while(left<right)
         {
             int length = Math.min(height[left],height[right]);
@@ -13,13 +11,8 @@ class Solution {
             area = length * width;
             maxArea = Math.max(maxArea,area);
              // Move the smaller height pointer
-             if(height[left]<height[right])
-             {
-                left++;
-             }
-             else{
-                right--;
-             }
+             if(height[left]<height[right]) left++;
+             else right--;
         }
         return maxArea;
     }
